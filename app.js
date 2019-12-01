c7d334ecff08bcf9d3538a01837dd9018ba4d102
daxwann <daxwann@gmail.com>
2019-11-30 18:11:19 -0800

    complete 3.3

--- app.js
+++ app.js
@@ -28,6 +28,17 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).send('Not Found');
+  }
+})
+
 app.get('/info', (req, res) => {
   const time = new Date();
   res.send(`<p>Phonebook has info for ${persons.length} people</p>\n<p>${time}</p>`)
