1bf16d23349fef8946e0fcbf8ddce15a7caa49a4
daxwann <daxwann@gmail.com>
2019-11-30 17:40:31 -0800

    complete 3.2

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  const time = new Date();
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>\n<p>${time}</p>`)
+})
+
 const PORT = 3001
 
 app.listen(PORT, () => {
